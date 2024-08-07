97b3f65e399aba231d16cd451e04e5a7ae6d53b1
mfuadfakhruzzaki <mfuadfakhruzzaki@students.undip.ac.id>
2024-08-07 19:31:22 +0700

    Initial commit

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,13 +1,11 @@
 import React from "react";
-
+import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Text, View } from "react-native";
-export default function Homescreen() {
+
+export default function Plus() {
   return (
     <SafeAreaView>
-      <View>
-        <Text>KONTOL BADAG</Text>
-      </View>
+      <Text>PROFILE</Text>
     </SafeAreaView>
   );
 }
