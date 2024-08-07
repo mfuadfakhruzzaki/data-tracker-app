27100f3ffdd977f563583b8b47cff218611bbc7b
Dhafin <rizkydhafin8615@gmail.com>
2024-08-07 14:43:34 +0700

    ok

---
+++ components/kontol.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export class kontol extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+
+export default kontol
